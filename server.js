64abcd5aefd8c99f75a7cef2da6375ae047b4b4a
rkr-dev <50330315+rkr-dev@users.noreply.github.com>
2022-08-08 19:31:19 -0700

    Server Setup
     - Server Setup

---
+++ server.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+dotenv.config({});
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from the root route' });
+});
+
+const PORT = process.env.PORT || 5500;
+const server = app.listen(PORT, () => {
+  console.log(`The Local server is running on port ${PORT}`);
+});
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error Occured : `, err.message);
+  server.close(() => process.exit(1));
+});
