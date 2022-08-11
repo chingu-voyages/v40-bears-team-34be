abd2f9c742017dce5eec27ef403b8dca2b71cee1
rkr-dev <50330315+rkr-dev@users.noreply.github.com>
2022-08-11 06:21:26 -0700

    Review Controllers
     - Review Controllers

---
+++ controllers/reviews.js
@@ -0,0 +1,56 @@
+import ErrorResponse from '../utils/errorResponse.js';
+import { asyncHandler } from '../middlewares/async.js';
+import { Review } from '../models/Review.js';
+import { Apartment } from '../models/Apartment.js';
+
+// @desc      Add a review
+// @route     POST /api/v1/apartments/:apartmentId/reviews
+// @access    Public as of now
+
+export const addReview = asyncHandler(async (req, res, next) => {
+  req.body.apartment = req.params.apartmentId;
+  // req.body.user = ``;
+
+  const apartment = await Apartment.findById(req.params.apartmentId);
+  if (!apartment) {
+    return next(new ErrorResponse(`No Apartment found with that Id`,404))
+  }
+  const review = await Review.create(req.body);
+
+  res.status(201).json({success:true,data:review})
+})
+
+// @desc      Get al reviews
+// @route     GET /api/v1/apartments/:apartmentId/reviews
+// @access    Public
+
+export const getApartmentReviews = asyncHandler(async (req, res, next) => {
+
+    const apartment = await Apartment.findById(req.params.apartmentId);
+    if (!apartment) {
+        return next(new ErrorResponse(`No Apartment found with that Id`, 404));
+    }
+    const review = await Review.find({})
+        .populate({ path: 'user', select: 'name' })
+        .populate({ path: 'apartment', select: 'name city ' });;
+
+    res.status(200).json({ success: true, data: review });
+});
+
+export const getReview = asyncHandler(async (req, res, next) => {
+    const apartment = await Apartment.findById(req.params.apartmentId);
+    if (!apartment) {
+        return next(new ErrorResponse(`No Apartment found with that Id`, 404));
+    }
+    const review = await Review.findById(req.params.reviewId)
+        .populate({ path: 'user', select: 'name' })
+        .populate({ path: 'apartment', select: 'name city ' });
+    if (!review) {
+        return next(new ErrorResponse(`No such review found`, 404));
+    }
+    res.status(200).json({ success: true, data: review });
+});
+
+
+
+
