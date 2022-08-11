e5f388611c868f5755467eadd8e6014a45d2a5ce
rkr-dev <50330315+rkr-dev@users.noreply.github.com>
2022-08-11 06:18:25 -0700

    Amenity Schema
     - Amenity Schema

---
+++ models/Amenity.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+export const AmenitySchema = new mongoose.Schema({
+    petFriendly: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    gated: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    InUnitWasherDryer: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    pool: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    fitnessCenter: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    parking: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+},{timestamps:true});
+
+export const Amenity = mongoose.model('Amenity', AmenitySchema);
